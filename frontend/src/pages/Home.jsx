173a9131f14cc293657b9400c06a03ea27fa56b7
Aleksandar Bursac <aleksbrsc@gmail.com>
2025-02-11 01:20:47 -0500

    feat(Home.jsx): About section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -82,46 +82,66 @@ const Home = () => {
     }, [handleKeyDown]);
 
     return (
-        <section id={styles.hero_section} ref={heroRef}>
-            <div id={styles.hero_container}>
-                <Canvas id={styles.hero_canvas} camera={{ fov: 10, position: [7, 1.75, 10], zoom: 1.5 }}>
-                    <Stage shadows={null} environment={null} intensity={0.01}>
-                        <ambientLight intensity={1} />
-                        <directionalLight position={[5, 5, 5]} intensity={1} />
-                        <directionalLight position={[-5, 5, 5]} intensity={0.5} />
-                        <Commodore moveToCenter={moveToCenter} targetSpeed={targetSpeed} />
-                        <CameraAnimation
-                            animateCamera={animateCamera}
-                            onAnimationEnd={() => {
-                                setShowTerminal(true);
-                                setEnableControls(true);
-                                setAnimateCamera(false);
-                            }}
-                            resetZoom={resetZoom}
-                        />
-                        <OrbitControls
-                            enabled={enableControls}
-                            target={[0, 0, 0]}
-                            minPolarAngle={0}
-                            maxPolarAngle={Math.PI / 2}
-                            enableZoom={false}
-                            enablePan={false}
-                            onStart={() => setTargetSpeed(0)}
-                            onEnd={() => setTargetSpeed(0.001)}
-                        />
-                        <EffectComposer>
-                            <DepthOfField focusDistance={0} focalLength={1} bokehScale={2} height={480} />
-                            {/* <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={320} /> */}
-                            <Noise opacity={0.1} />
-                            <Vignette eskil={false} offset={0.1} darkness={1.1} />
-                            <ToneMapping adaptive resolution={256}/>
-                        </EffectComposer>
-                    </Stage>
-                </Canvas>
-                {!showTerminal && <div id={styles.hero_hint}>Press enter to start.</div>}
-                {showTerminal && <Terminal />}
-            </div>
-        </section>
+        <>
+            <section id={styles.hero_section} ref={heroRef}>
+                <div id={styles.hero_container}>
+                    <Canvas id={styles.hero_canvas} camera={{ fov: 10, position: [7, 1.75, 10], zoom: 1.5 }}>
+                        <Stage shadows={null} environment={null} intensity={0.01}>
+                            <ambientLight intensity={1} />
+                            <directionalLight position={[5, 5, 5]} intensity={1} />
+                            <directionalLight position={[-5, 5, 5]} intensity={0.5} />
+                            <Commodore moveToCenter={moveToCenter} targetSpeed={targetSpeed} />
+                            <CameraAnimation
+                                animateCamera={animateCamera}
+                                onAnimationEnd={() => {
+                                    setShowTerminal(true);
+                                    setEnableControls(true);
+                                    setAnimateCamera(false);
+                                }}
+                                resetZoom={resetZoom}
+                            />
+                            <OrbitControls
+                                enabled={enableControls}
+                                target={[0, 0, 0]}
+                                minPolarAngle={0}
+                                maxPolarAngle={Math.PI / 2}
+                                enableZoom={false}
+                                enablePan={false}
+                                onStart={() => setTargetSpeed(0)}
+                                onEnd={() => setTargetSpeed(0.001)}
+                            />
+                            <EffectComposer>
+                                <DepthOfField focusDistance={0} focalLength={1} bokehScale={2} height={480} />
+                                {/* <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={320} /> */}
+                                <Noise opacity={0.1} />
+                                <Vignette eskil={false} offset={0.1} darkness={1.1} />
+                                <ToneMapping adaptive resolution={256}/>
+                            </EffectComposer>
+                        </Stage>
+                    </Canvas>
+                    {!showTerminal && <div id={styles.hero_hint}>Press enter to start.</div>}
+                    {showTerminal && <Terminal />}
+                </div>
+            </section>
+            <section id={styles.about_section}>
+                <div id={styles.about_container}>
+                    <h1>SHERIDAN <span>COMPUTER</span> SCIENCE <span>CLUB</span></h1>
+                    <div id={styles.about_content}>
+                        <p>
+                            The SCSC is more than just a club — it's a thriving community where CS students learn,
+                            grow, and build together. We're proud to have some of Sheridan's top developers
+                            contributing to important projects for the college.
+                        </p>
+                        <p>
+                            We're committed to empowering all CS students by providing resources, workshops, 
+                            and opportunities to collaborate on exciting initiatives. Join us and become part 
+                            of something special!
+                        </p>
+                        <p id={styles.quote_author}>- SCSC team</p>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
